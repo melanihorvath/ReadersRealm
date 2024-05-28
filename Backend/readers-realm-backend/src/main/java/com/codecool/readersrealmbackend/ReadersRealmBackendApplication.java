032454e618ac1b1f687400828540c150abd00873
melanihorvath <144622336+melanihorvath@users.noreply.github.com>
2024-05-28 20:21:21 +0200

    add backend project skeleto and database confign

---
+++ Backend/readers-realm-backend/src/main/java/com/codecool/readersrealmbackend/ReadersRealmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.readersrealmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersRealmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadersRealmBackendApplication.class, args);
+    }
+
+}
